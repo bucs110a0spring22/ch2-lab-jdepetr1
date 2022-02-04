0bae401c12d8ffe4331c8f4f7d9a6a581cd561d2
Jdepetr1 P <49722708+jdepetr1@users.noreply.github.com>
2022-02-04 02:43:56 +0000

    README not done main part B almost done

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
+
+classes_per_week = 12 # I created
+print("Classes per week:", classes_per_week, type(classes_per_week))
+
+
 cost_per_week = ((tuition / classes) / weeks)
+cost_per_class = cost_per_week / classes_per_week # I created
+print("Cost per class:", cost_per_class, type(cost_per_class))
+
 print("Cost per week:", cost_per_week)
 
 
 #Part B
+randomList = ["Chocolate", 493, 12.21, "Wow!", 96]
+randomResult = random.choice(randomList)
+print("Random choice result:", randomResult, type(randomResult))
